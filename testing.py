678c517d86d22b2a75423e206f490534bed0f101
foryourselfand <foryourselfand@gmail.com>
2021-03-21 13:31:52 +0300

    feat: get and extract commits

--- testing.py
+++ testing.py
@@ -1,4 +1,5 @@
 import json
+import zipfile
 from pprint import pprint
 
 import requests
@@ -46,9 +47,67 @@ def get_task(variant: int):
     return json.loads(json_raw)
 
 
+def get_commit(variant: int, commit: int):
+    cookies = {
+        'JSESSIONID': 'pN6_CGtbS1GB9OsVpV8UrT_D8rdm77S97X23vKgR.helios',
+    }
+    
+    headers = {
+        'Connection': 'keep-alive',
+        'Cache-Control': 'max-age=0',
+        'sec-ch-ua': '"Google Chrome";v="89", "Chromium";v="89", ";Not A Brand";v="99"',
+        'sec-ch-ua-mobile': '?0',
+        'Upgrade-Insecure-Requests': '1',
+        'Origin': 'https://se.ifmo.ru',
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 11_2_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+        'Sec-Fetch-Site': 'same-origin',
+        'Sec-Fetch-Mode': 'navigate',
+        'Sec-Fetch-User': '?1',
+        'Sec-Fetch-Dest': 'document',
+        'Referer': 'https://se.ifmo.ru/courses/software-engineering-basics',
+        'Accept-Language': 'ru,en-US;q=0.9,en;q=0.8',
+    }
+    
+    params = (
+        ('p_p_id', 'selab2_WAR_seportlet'),
+        ('p_p_lifecycle', '2'),
+        ('p_p_state', 'normal'),
+        ('p_p_mode', 'view'),
+        ('p_p_cacheability', 'cacheLevelPage'),
+    )
+    
+    data = {
+        'variant': variant,
+        'commit': commit
+    }
+    
+    response = requests.post('https://se.ifmo.ru/courses/software-engineering-basics', headers=headers, params=params, cookies=cookies, data=data)
+    
+    directory_to_extract_to = f'res/{variant}/commit{commit}'
+    path_to_zip_file = f'{directory_to_extract_to}.zip'
+    
+    with open(path_to_zip_file, 'wb+') as zip_file:
+        zip_file.write(response.content)
+    
+    with zipfile.ZipFile(path_to_zip_file, 'r') as zip_reference:
+        zip_reference.extractall(directory_to_extract_to)
+
+
 def main() -> None:
-    data = get_task(273826)
+    variant = 273826
+    
+    data = get_task(variant=variant)
     pprint(data)
+    
+    commits = []
+    for branch_key, branch_value in data.items():
+        commits_current = branch_value['commits']
+        commits += commits_current
+    
+    for commit in commits:
+        get_commit(variant=variant, commit=commit)
 
 
 if __name__ == '__main__':
